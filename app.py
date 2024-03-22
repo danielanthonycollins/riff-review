1812bd338738737bb0ebc07757966c9b5c0cd8a5
Daniel Collins <danielanthonycollins@gmail.com>
2024-03-22 19:49:03 +0000

    fix: Spelling error in app.py stopping app running from workspace

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import (Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-if os.path.exists("eny.py"):
+if os.path.exists("env.py"):
     import env
 
 
