c753a2c965553b61582d8a14fb9fc36f556eed90
Daniel Collins <danielanthonycollins@gmail.com>
2024-03-28 15:16:03 +0000

    Add and connect edit review button

--- app.py
+++ app.py
@@ -119,6 +119,13 @@ def new_review():
     genres = mongo.db.genres.find().sort("genre_name", 1)
     return render_template("new_review.html", genres=genres)
 
+
+@app.route("/edit_review/<review_id>", methods=["GET", "POST"])
+def edit_review(review_id):
+    review = mongo.db.reviews.find_one({"_id": ObjectId(review_id)})
+    genres = mongo.db.genres.find().sort("genre_name", 1)
+    return render_template("edit_review.html", review=review, genres=genres)
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
